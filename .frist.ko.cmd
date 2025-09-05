9d5dfe5696d5ce97354818656fc8edc58f2a5810
leey <leey22592@gmail.com>
2025-09-05 21:59:38 +0800

    delete other

--- .frist.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/leeyan/learning/frist.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -z noexecstack   --build-id  -T ./scripts/module-common.lds -o /home/leeyan/learning/frist.ko /home/leeyan/learning/frist.o /home/leeyan/learning/frist.mod.o;  true
